143f51009c7d322841b8063a9b964c4046e4ee98
greymanhw <greymanhw@gmail.com>
2019-09-09 23:26:19 -0700

    Removed wifi bits to get the LEDs working

--- rgb_led_over_wifi/rgb_led_over_wifi.ino
+++ rgb_led_over_wifi/rgb_led_over_wifi.ino
@@ -15,37 +15,137 @@
  *  server_ip is the IP address of the ESP32, will be 
  *  printed to Serial when the module is connected.
 */
-#include <WiFi.h>
+#include <string> 
+#include <stdint.h>
 
-const char* ssid = "";
-const char* password = "";
-WiFiServer server(80);
-
- #define MAX_LED_VALUE 255
-// use first 3 channels of 16 channels (started from zero)
-#define LEDC_CHANNEL_0_R  0
-#define LEDC_CHANNEL_1_G  1
-#define LEDC_CHANNEL_2_B  2
-
+#define MAX_LED_VALUE 255
 #define RESOLUTION 8
 #define LEDC_BASE_FREQ 5000
 
 // LED PINs
-#define LED_PIN_R 23
-#define LED_PIN_G 22
-#define LED_PIN_B 21
+const int pins[] = {23,22,21,19,18,17,25,26,27};
+const int pinsLength = sizeof(pins) / sizeof(*pins);
+const int ledCount = pinsLength / 3;
 
-/*
-Because Multiple libraries were found for "WiFi.h",
-Arduino IDE will: 
- Used: ...\Arduino\hardware\espressif\esp32\libraries\WiFi
- Not used: C:\Program Files (x86)\Arduino\libraries\WiFi
+// [patterns defined][[led count][led channel]
+unsigned long patternArray[23][ledCount][3] = {
+  { 
+    {0xff,0x00,0x00},
+    {0x00,0xff,0x00},
+    {0x00,0x00,0xff},
+  },
+  { 
+    {0xee,0x00,0x00},
+    {0x00,0xee,0x00},
+    {0x00,0x00,0xee},
+  },
+  { 
+    {0xdd,0x00,0x00},
+    {0x00,0xdd,0x00},
+    {0x00,0x00,0xdd},
+  },
+  { 
+    {0xcc,0x00,0x00},
+    {0x00,0xcc,0x00},
+    {0x00,0x00,0xcc},
+  },
+  { 
+    {0xbb,0x00,0x00},
+    {0x00,0xbb,0x00},
+    {0x00,0x00,0xbb},
+  },
+  { 
+    {0xaa,0x00,0x00},
+    {0x00,0xaa,0x00},
+    {0x00,0x00,0xaa},
+  },
+  { 
+    {0x99,0x00,0x00},
+    {0x00,0x99,0x00},
+    {0x00,0x00,0x99},
+  },
+  { 
+    {0x88,0x00,0x00},
+    {0x00,0x88,0x00},
+    {0x00,0x00,0x88},
+  },
+  { 
+    {0x77,0x00,0x00},
+    {0x00,0x77,0x00},
+    {0x00,0x00,0x77},
+  },
+  { 
+    {0x66,0x00,0x00},
+    {0x00,0x66,0x00},
+    {0x00,0x00,0x66},
+  },
+  { 
+    {0x55,0x00,0x00},
+    {0x00,0x55,0x00},
+    {0x00,0x00,0x55},
+  },
+  { 
+    {0x44,0x00,0x00},
+    {0x00,0x44,0x00},
+    {0x00,0x00,0x44},
+  },
+  { 
+    {0x55,0x00,0x00},
+    {0x00,0x55,0x00},
+    {0x00,0x00,0x55},
+  },
+  { 
+    {0x66,0x00,0x00},
+    {0x00,0x66,0x00},
+    {0x00,0x00,0x66},
+  },
+  { 
+    {0x77,0x00,0x00},
+    {0x00,0x77,0x00},
+    {0x00,0x00,0x77},
+  },
+  { 
+    {0x88,0x00,0x00},
+    {0x00,0x88,0x00},
+    {0x00,0x00,0x88},
+  },
+  { 
+    {0x99,0x00,0x00},
+    {0x00,0x99,0x00},
+    {0x00,0x00,0x99},
+  },
+  { 
+    {0xaa,0x00,0x00},
+    {0x00,0xaa,0x00},
+    {0x00,0x00,0xaa},
+  },
+  { 
+    {0xbb,0x00,0x00},
+    {0x00,0xbb,0x00},
+    {0x00,0x00,0xbb},
+  },
+  { 
+    {0xcc,0x00,0x00},
+    {0x00,0xcc,0x00},
+    {0x00,0x00,0xcc},
+  },
+  { 
+    {0xdd,0x00,0x00},
+    {0x00,0xdd,0x00},
+    {0x00,0x00,0xdd},
+  },
+  { 
+    {0xee,0x00,0x00},
+    {0x00,0xee,0x00},
+    {0x00,0x00,0xee},
+  },
+  { 
+    {0xff,0x00,0x00},
+    {0x00,0xff,0x00},
+    {0x00,0x00,0xff},
+  },
+};
 
-And exit with error:
-'min' was not declared in this scope
-
-Here's a min() to avoid that
- */
 uint32_t min(uint32_t num1, uint32_t num2){
   if(num1 < num2) {
     return num1;
@@ -57,110 +157,31 @@ uint32_t min(uint32_t num1, uint32_t num2){
 void setup() {
   Serial.begin(115200);
   delay(10);
-  
+  Serial.println("There are " + String(ledCount) + " LEDs");
+
   // Setup timer and attach timer to a led pins
-  ledcSetup(LEDC_CHANNEL_0_R, LEDC_BASE_FREQ, RESOLUTION);
-  ledcAttachPin(LED_PIN_R, LEDC_CHANNEL_0_R);
-  ledcSetup(LEDC_CHANNEL_1_G, LEDC_BASE_FREQ, RESOLUTION);
-  ledcAttachPin(LED_PIN_G, LEDC_CHANNEL_1_G);
-  ledcSetup(LEDC_CHANNEL_2_B, LEDC_BASE_FREQ, RESOLUTION);
-  ledcAttachPin(LED_PIN_B, LEDC_CHANNEL_2_B);
-
-  // Connect to WiFi network
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  
-  WiFi.begin(ssid, password);
-  
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  int channel = 0;
+  for (int i = 0; i < pinsLength; i++){
+    ledcSetup(channel, LEDC_BASE_FREQ, RESOLUTION);
+    ledcAttachPin(pins[channel], channel);
+    channel++;
   }
-  Serial.println("");
-  Serial.println("WiFi connected");
-  
-  // Start the server
-  server.begin();
-  Serial.println("Server started");
-
-  // Print the IP address
-  Serial.print(WiFi.localIP());
-  Serial.println("/rgb/000000/");
 }
 
 void loop() {
-  // Check if a client has connected
-  WiFiClient client = server.available();
-  if (!client) {
-    return;
-  }
-  
-  // Wait until the client sends some data
-  Serial.println("new client");
-  while(client.connected()){
-    if(client.available()){
-  
-      // Read the first line of the request
-      String req = client.readStringUntil('\r');
-      Serial.println(req);
-  
-      // Match the request
-      unsigned long valR, valG, valB;
-      String subStringR, subStringG, subStringB;
-      int index = req.indexOf("/rgb/");
-      if(index != -1){
-        if(req.charAt(index+11)=='/'){
-          subStringR = req.substring(index+5, index+7);
-          subStringG = req.substring(index+7, index+9);
-          subStringB = req.substring(index+9, index+11);
-          Serial.println("R: " + subStringR);
-          Serial.println("G: " + subStringG);
-          Serial.println("B: " + subStringB);
-    
-          char red_hex[3]; 
-          strcpy(red_hex, subStringR.c_str()); 
-          valR = strtoul(red_hex, NULL, 16);
-          char green_hex[3]; 
-          strcpy(green_hex, subStringG.c_str()); 
-          valG = strtoul(green_hex, NULL, 16);
-          char blue_hex[3]; 
-          strcpy(blue_hex, subStringB.c_str()); 
-          valB = strtoul(blue_hex, NULL, 16);
-          
-          Serial.println("valR: " + String(valR));
-          Serial.println("valG: " + String(valG));
-          Serial.println("valB: " + String(valB));
-          
-        } else {
-          Serial.println("Not terminated with /");
-          client.stop();
-          return;
+      int channel;
+      int patternArrayLength = sizeof(patternArray) / sizeof(*patternArray);
+      for (int i = 0; i < patternArrayLength; i++){
+        // i is the row of the patternArray we are working on, each row represents 3 leds
+        channel = 0;
+        for (int j = 0; j < ledCount; j++) {
+          // j is the LED we are working on
+          int patternArraySubLength = sizeof(patternArray[i]) / sizeof(*patternArray[i]);
+          for (int k = 0; k < patternArraySubLength; k++) {
+            ledcWrite(channel, min(255 - patternArray[i][j][k], MAX_LED_VALUE));
+            channel++;
+          }
         }
-      } else {
-        Serial.println("No /rgb/ found");
-        client.stop();
-        return;
+        delay(75);
       }
-
-      // Set GPIOs according to the request
-      // No check valid of the requested setting
-      // Probably should add a max check with the (255 - valX) inversion
-      ledcWrite(LEDC_CHANNEL_0_R, min(255 - valR, MAX_LED_VALUE));
-      ledcWrite(LEDC_CHANNEL_1_G, min(255 - valG, MAX_LED_VALUE));
-      ledcWrite(LEDC_CHANNEL_2_B, min(255 - valB, MAX_LED_VALUE));
-  
-      // Prepare the response
-      String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\nGPIOs of RGB is now ";
-      s += String(valR) +":" + String(valG) + ":" + String(valB);
-      s += "</html>\n";
-
-      // Send the response to the client
-      client.print(s);
-      delay(1);
-      client.stop();
-      Serial.println("Client disconnected");
-    }
-  }
 }
